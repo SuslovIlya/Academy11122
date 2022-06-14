580c7f99390ed405b822013d1127c50fcab14931
SuslovIlya <suslovvilya@gmail.com>
2022-06-14 21:24:47 +0300

    first commit

---
+++ Academy/src/main/java/by/academy/classwork/lesson1/Aplication.java
@@ -0,0 +1,22 @@
+package by.academy.classwork.lesson1;
+
+public class Aplication {
+		
+    }
+
+	  {
+    Cat cat1=new Cat(); }
+	
+	Cat cat2=new Cat();
+	
+	
+	Cat.eat();
+	}{
+	
+	Cat.walk();
+	}{
+	
+	
+	
+
+	
