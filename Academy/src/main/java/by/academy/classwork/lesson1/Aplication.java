251e006f01eb07e0316290fefaf2d9f17f138c1a
SuslovIlya <107502693+SuslovIlya@users.noreply.github.com>
2022-06-15 20:48:05 +0300

    Update Aplication.java

--- Academy/src/main/java/by/academy/classwork/lesson1/Aplication.java
+++ Academy/src/main/java/by/academy/classwork/lesson1/Aplication.java
@@ -5,16 +5,63 @@ public class Aplication {
     }
 
 	  {
-    Cat cat1=new Cat(); }
+		  
+        int age;
+
+	String nickname = кот1;
 	
-	Cat cat2=new Cat();
+	double money;
+
+	public Cat(string nickname); {
+		this.nickname=nickname;
+		initials=nickname.charAt(0);
+		}
+
+	boolean isHomeAnimal;
+
+	public void grow() {
+		age + = 1;
+	}
+
+	public void sleep() {
+		System.out.println("Кот спит");
+	}
+
+	public void eat() {
+		System.out.println("Кот ест");
+	}
+
+	public void walk() {
+		System.out.println("Кот гуляет");
+	}
+
+	public Cat() {
+	}
 	
-	
-	Cat.eat();
-	}{
-	
-	Cat.walk();
-	}{
+	public double getMoney() {
+		return money;
+	}
+
+	public void setMoney(double money) {
+		this.money = money;
+	}
+
+	public char getInitials() {
+		return initials;
+	}
+
+	public void setInitials(char initials) {
+		this.initials = initials;
+	}
+
+	public boolean getIsHomeAnimal() {
+		return isHomeAnimal;
+	}
+
+	public void setIsHomeAnimal(boolean isHomeAnimal) {
+		this.isHomeAnimal = isHomeAnimal;
+	}
+}
 	
 	
 	
